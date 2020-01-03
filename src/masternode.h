00da4f02d6e1be8af88e9ee2f47cb8f6bb074ddf
MitchellMint <github@mitchellmint.com>
2020-01-03 01:01:51 -0600

    Update masternode.h

--- src/masternode.h
+++ src/masternode.h
@@ -136,7 +136,7 @@ struct masternode_info_t
 };
 
 //
-// The Masternode Class. For managing the Darksend process. It contains the input of the 5000 XTP, signature to prove
+// The Masternode Class. For managing the Darksend process. It contains the input of the 5000 TRUST, signature to prove
 // it's the one who own that ip address and code for calculating the payment election.
 //
 class CMasternode
