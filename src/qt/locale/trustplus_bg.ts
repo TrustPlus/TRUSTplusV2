c5bd8e0ffd221498c11d98badaf2d27c429e1098
MitchellMint <github@mitchellmint.com>
2020-04-07 22:09:55 -0500

    Update trustplus_bg.ts

--- src/qt/locale/trustplus_bg.ts
+++ src/qt/locale/trustplus_bg.ts
@@ -172,7 +172,7 @@
         <translation>Потвърдете на шифрирането на портфейла</translation>
     </message>
     <message>
-        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR XTP&lt;/b&gt;!</source>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR TRUST&lt;/b&gt;!</source>
         <translation>Внимание: Ако шифрирате вашия портфейл и загубите паролата си,  &lt;b&gt;ЩЕ ЗАГУБИТЕ ВСИЧКИ XTP МОНЕТИ!&lt;/b&gt;!</translation>
     </message>
     <message>
